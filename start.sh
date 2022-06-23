bcf9b7fbe9cc350cbe5743b5e5a378ca77469b9f
Anagh VM <93310059+Anaghvm12@users.noreply.github.com>
2022-06-23 15:28:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Aadhi000/Ajax.git /Ajax /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Ajax
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
-echo "MTG bot Starting....🔥"
+echo " Starting Mtg....🔥"
 python3 bot.py
