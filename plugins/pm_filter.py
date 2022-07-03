9c82e158132780e04fcae857fda650c4f0ac4b34
Anagh VM <93310059+Anaghvm12@users.noreply.github.com>
2022-07-03 15:37:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1295,8 +1295,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
-       )
-   elif query.data.startswith("setgs"):
+        )
+    elif query.data.startswith("setgs"):
         ident, set_type, status, grp_id = query.data.split("#")
         grpid = await active_connection(str(query.from_user.id))
 
