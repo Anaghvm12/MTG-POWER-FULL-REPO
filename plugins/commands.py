211d25d3a62b4bced52711aa9ab61455c48115e1
Anagh VM <93310059+Anaghvm12@users.noreply.github.com>
2022-07-05 10:30:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,13 +4,16 @@ import random
 import asyncio
 from Script import script
 from pyrogram import Client, filters
-from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
+from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, CUSTOM_FILE_CAPTION, BATCH_FILE_CAPTION, PROTECT_CONTENT
-from utils import get_size, is_subscribed, temp
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
+from database.connections_mdb import active_connection
 import re
+import json
+import base64
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
