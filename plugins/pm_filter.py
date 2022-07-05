a1cadeae208e13efa8c7fd57ff42c5af2fe1c357
Anagh VM <93310059+Anaghvm12@users.noreply.github.com>
2022-07-05 20:20:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1401,7 +1401,7 @@ async def auto_filter(client, msg, spoll=False):
                 ),
                 InlineKeyboardButton(
                     text=f"📂 [{get_size(file.file_size)}] {file.file_name}", 
-                        callback_data=f'{pre}#{file.file_id}#{msg.from_user.id if msg.from_user is not None else 0}'',
+                        callback_data=f'{pre}#{file.file_id}#{msg.from_user.id if msg.from_user is not None else 0}',
                 ),
             ]
             for file in files
